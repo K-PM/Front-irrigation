b404f5c7af9dbdfe7f13ab21db539d54ec82a3d2
Mauricio Castillo <108595587+CassDeLaRy@users.noreply.github.com>
2023-03-27 09:36:50 -0600

    THE THIRD UPDATE - 3.0v
    There is a new apartment where you can print your document and see another panel of irrigation with humedity and stuff

---
+++ lista-plantas/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './components/Title'
+import Title from './components/Title'
+// import PlantCard from './components/PlantCard'
+import PlantDashboard from './components/PlantDashboard'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Title/>
+      {/* <PlantCard/> */}
+      <PlantDashboard/>
+    </div>
+  )
+}
+
+export default App
